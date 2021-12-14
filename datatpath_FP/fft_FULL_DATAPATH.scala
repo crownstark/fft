d8a95b7fb211138e7463bfaba2daed359675a21e
crownstark <64046038+crownstark@users.noreply.github.com>
2021-12-14 02:16:25 -0500

    Add files via upload

--- datatpath_FP/fft_FULL_DATAPATH.scala
+++ datatpath_FP/fft_FULL_DATAPATH.scala
@@ -197,7 +197,7 @@ class BF_blocks_FP extends Module {
   val io = IO(new Bundle {
     val in = Input(Vec(8, FixedPoint(32.W, 16.BP)))
     val out = Output(Vec(8, FixedPoint(32.W, 16.BP)))
-    val wn = Input(FixedPoint(32.W, 16.BP))
+    //val wn = Input(FixedPoint(32.W, 16.BP))
     val stage_counter = Input(UInt(2.W))
     val out_en = Input(UInt(1.W))
   })
@@ -246,7 +246,7 @@ class BF_blocks_FP extends Module {
 
 class mainRF_redesign_FP extends Module {
   val io = IO(new Bundle {
-    val in = Input(Vec(8, FixedPoint(32.W, 16.BP)))
+    //val in = Input(Vec(8, FixedPoint(32.W, 16.BP)))
     val out = Output(Vec(8, FixedPoint(32.W, 16.BP)))
     val load_en = Input(UInt(1.W))
     val sel = Input(UInt(1.W))
@@ -291,6 +291,158 @@ class mainRF_redesign_FP extends Module {
   }
 }
 
+// one input, 4 output 4x32 Reg File -- serve as even/odd reg files
+class regFile4x32_redesign_FP extends Module {
+  val io = IO(new Bundle {
+    val stage_counter = Input(UInt(2.W))
+    val in = Input(Vec(4, FixedPoint(32.W, 16.BP)))
+    val out_en = Input(UInt(1.W))
+    val wr_en = Input(UInt(1.W))
+    val out = Output(Vec(4, FixedPoint(32.W, 16.BP)))
+  })
+  
+  for (i <- 0 until 4){
+    io.out(i) := 0.F(32.W, 16.BP)
+  }
+  
+  val reg = RegInit(VecInit(Seq.fill(4)(0.F(32.W, 16.BP))))
+  val rdaddr = RegInit(VecInit(Seq.fill(4)(0.U)))
+  
+  when(io.wr_en.asBool){
+    for (i <- 0 until 4){
+      reg(i) := io.in(i)
+    }
+  }
+  
+    // logic based on stage_counter that outputs from the four even/odd outputs
+  switch(io.stage_counter){
+    is(0.U){
+      rdaddr(0) := 0.U(2.W)
+      rdaddr(1) := 2.U(2.W)
+      rdaddr(2) := 1.U(2.W)
+      rdaddr(3) := 3.U(2.W)
+    }
+    is(1.U){
+      rdaddr(0) := 0.U(2.W)
+      rdaddr(1) := 1.U(2.W)
+      rdaddr(2) := 2.U(2.W)
+      rdaddr(3) := 3.U(2.W)
+    }
+    is(2.U){
+      rdaddr(0) := 0.U(2.W)
+      rdaddr(1) := 2.U(2.W)
+      rdaddr(2) := 1.U(2.W)
+      rdaddr(3) := 3.U(2.W)
+    }
+    is(3.U){
+      rdaddr(0) := 0.U(2.W)
+      rdaddr(1) := 2.U(2.W)
+      rdaddr(2) := 1.U(2.W)
+      rdaddr(3) := 3.U(2.W)
+    }
+   }
+
+   when(io.out_en.asBool){
+     for(i <- 0 until 4){
+       io.out(i) := reg(rdaddr(i))
+     }
+   }
+}
+
+
+
+// TODO: 12/14/21 1:10 AM COMPILES
+// TODO: WRITE A PEEKPOKE TEST WITH HANDPOKING THE CONTROL SIGNALS
+// TODO: 12/14/21 2:15 AM PEEK POKE RUNS BUT WRONG VALUES
+// TODO: STAGES 0 and 2 ARE EMPTY
+class fft_FULL_DATAPATH extends Module {
+  val io = IO(new Bundle {
+    // signals for even/odd 4x32 RF blocks
+    val even_outen = Input(UInt(1.W))
+    val even_wren = Input(UInt(1.W))
+    val odd_outen = Input(UInt(1.W))
+    val odd_wren = Input(UInt(1.W))
+    // BF blocks signals
+    val bf_outen = Input(UInt(1.W))
+    // mainRF redesign signals
+    val main_loaden = Input(UInt(1.W))
+    val main_outen = Input(UInt(1.W))
+    val main_sel = Input(UInt(1.W))
+    val from_ROM = Input(Vec(8, FixedPoint(32.W, 16.BP)))
+    // stage counter -- will connect to several modules
+    val stage_counter = Input(UInt(2.W))
+    // FINAL OUT
+    val out = Output(Vec(8, FixedPoint(32.W, 16.BP)))
+  })
+  
+  
+  // initialize out
+  for (i <- 0 until 8){
+    io.out(i) := 0.F(32.W, 16.BP)
+  }
+  
+  // bring in main reg file, REDESIGNED
+  val mainRF = Module(new mainRF_redesign_FP())
+  
+  // bring in TWO 4x32 reg files, EVEN/ODD arrays
+  // read addresses will CHANGE based on STAGE
+  val even = Module(new regFile4x32_redesign_FP())
+  val odd = Module(new regFile4x32_redesign_FP())
+  
+  // bring in BF blocks
+  val bf = Module(new BF_blocks_FP())
+  
+  // BF control signals
+  bf.io.out_en := io.bf_outen
+  bf.io.stage_counter := io.stage_counter   // wn produced based on stage
+  
+  
+  //main reg file control signal connections
+  mainRF.io.load_en := io.main_loaden
+  mainRF.io.out_en := io.main_outen
+  mainRF.io.from_ROM := io.from_ROM
+  // sel high is from BF, sel low is from ROM
+  mainRF.io.sel := io.main_sel
+  // from BF input to mainreg
+  for (i <- 0 until 8){
+    mainRF.io.from_BF(i) := bf.io.out(i)
+  }
+  
+  
+  // even/odd control signal connections
+  even.io.wr_en := io.even_wren
+  even.io.out_en := io.even_outen
+  even.io.stage_counter := io.stage_counter
+  odd.io.wr_en := io.odd_wren
+  odd.io.out_en := io.odd_outen
+  odd.io.stage_counter := io.stage_counter
+  // 0 2 4 6 on the 0 1 2 3 outputs of main RF
+  for (i <- 0 until 4){
+    even.io.in(i) := mainRF.io.out(i)
+  }
+  // 1 3 5 7 on the 4 5 6 7 outputs of main RF
+  for (i <- 0 until 4){
+    odd.io.in(i) := mainRF.io.out(i+4)
+  }
+  
+  // input of the BF blocks comes straight from even/odd blocks
+  for (i <- 0 until 4){
+    bf.io.in(i) := even.io.out(i)
+  }
+  for(i <- 0 until 4){
+    bf.io.in(i+4) := odd.io.out(i)
+  }
+  
+  // final output assignment
+  for (i <- 0 until 8){
+    io.out(i) := bf.io.out(i)
+  }
+}
+  
+  
+  
+  
+    
 
     
   
